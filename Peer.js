a49e9b00ad004ee62f888fd6c04b1a28008d942a
melkml <mmldn7@gmail.com>
2022-03-21 02:31:21 -0300

    Add p2p chat code.

---
+++ Peer.js
@@ -0,0 +1,45 @@
+
+
+const net = require("net");
+module.exports = class Peer {
+  constructor(port) {
+    this.port = port;
+    this.connections = [];
+    const server = net.createServer( (socket) => {
+        this.onSocketConnected(socket)});
+    server.listen(port, () => console.log("Ouvindo porta " + port) )
+  }
+
+  connectTo(address) {
+    if(address.split(":").length !== 2)
+        throw Error("O endereço do outro peer deve ser composto por host:port ");
+    const [ host, port ] = address.split(":");
+    const socket = net.createConnection({ port, host }, () =>
+    this.onSocketConnected(socket));
+}
+
+   onSocketConnected(socket) {
+       console.log("Nova conexão");
+       this.connections.push(socket);
+       socket.on('data', (data) =>
+         this.onData(socket, data));
+       this.onConnection(socket);
+       socket.on('close', () => {
+        this.connections = this.connections.filter( conn => {
+            return conn !== socket;
+        })
+    });
+ }
+
+   onConnection(socket) {}
+
+   onData(socket, data) {
+      console.log("received: ", data.toString())
+}
+
+   broadcast(data) {
+      this.connections.forEach( socket => socket.write(data) )
+}
+
+   
+}
---
+++ index.js
@@ -0,0 +1,34 @@
+if(!process.env.PORT)
+    throw Error("Variável de ambiente PORT não informada");
+const port = process.env.PORT;
+const sha = require('sha256');
+const timestamp = Date.now();
+const randomNumber = Math.floor( (Math.random() * 10000) + 1000 )
+const myKey = sha(port + "" + timestamp + "" + randomNumber );
+const Peer = require("./Peer");
+const peer = new Peer(port);
+
+process.argv.slice(2).forEach( anotherPeerAddress => peer.connectTo(anotherPeerAddress) );
+peer.onConnection = socket => {
+    const message = "Hi !! I'm on port " + port;
+    const signature = sha(message + myKey + Date.now());
+    receivedMessageSignatures.push(signature);
+    const firstPayload = {signature, message}
+    socket.write(JSON.stringify(firstPayload))
+};
+process.stdin.on('data', data => {
+    const message = data.toString().replace(/\n/g, "");
+    const signature = sha(message + myKey + Date.now());
+    receivedMessageSignatures.push(signature);
+    peer.broadcast(JSON.stringify({ signature, message}));
+});
+const receivedMessageSignatures = [ myKey ];
+peer.onData = (socket, data) => {
+    const json = data.toString();
+    const payload = JSON.parse(json);
+    if(receivedMessageSignatures.includes(payload.signature))
+        return;
+    receivedMessageSignatures.push(payload.signature)
+    console.log("recebido> ", payload.message)
+    peer.broadcast(json);
+};
